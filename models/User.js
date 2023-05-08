dcdd501b4c29f0261ea17940786c2b27f4868974
Ikenna <iykee0333@gmail.com>
2023-05-08 12:28:50 +0100

    user schema edit

--- models/User.js
+++ models/User.js
@@ -41,4 +41,5 @@ const UserSchema = new mongoose.Schema(
 	{ timestamps: true }
 );
 
-export default User = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
+export default User;
